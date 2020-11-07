f6b3b7fd7d4a85eefd04d723f7a7e48891583d47
Mamun <mamun.cse63@gmail.com>
2020-11-07 17:07:49 +0600

    Text & Colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quasi similique fugit deserunt eligendi rem minus doloremque modi qui, beatae dignissimos dolorem numquam suscipit. Nobis ducimus ullam est vero eligendi.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quasi similique fugit deserunt eligendi rem minus doloremque modi qui, beatae dignissimos dolorem numquam suscipit. Nobis ducimus ullam est vero eligendi.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is Headline</p>
+    <p class="subheading font-weight-bold">This is Subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
