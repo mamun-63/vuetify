3c4304dbd46cf90f7e4811481cd0702602e2321c
Mamun <mamun.cse63@gmail.com>
2020-11-07 19:28:15 +0600

    Breakpoints & Visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,12 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <v-btn class="pink white--text ml-3">Click me</v-btn>
-    <v-btn depressed color="pink ml-3">Click me</v-btn>
-    <v-btn text color="pink ml-3">Click me</v-btn>  
-    <v-btn primary class="ml-3">Click me</v-btn>
+    <!-- Breakpoints means the display/device size, Visibility means if we only want to show it in large device/ small etc that kind of stuff-->
     
-    <br><br>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+    <v-btn class="hidden-sm-only">click me</v-btn>
 
-    <v-btn large class="pink white--text ml-3">
-      <v-icon large left>mdi-email</v-icon>
-      <span>email</span>
-    </v-btn>
-    
-    <v-btn small class="pink white--text ml-3">
-      <v-icon small left>mdi-email</v-icon>
-      <span>email</span>
-    </v-btn>
-
-    <v-btn class="pink white--text mx-3">
-      <span>email</span>
-      <v-icon right>mdi-email</v-icon>
-    </v-btn>
-
-    <v-btn fab dark color="purple">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
   </div>
 </template>
 
