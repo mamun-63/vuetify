f9fbdc2f5240267d3170dd8c33213f69bb7e8b13
Mamun <mamun.cse63@gmail.com>
2020-11-07 18:27:08 +0600

    Buttons & Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quasi similique fugit deserunt eligendi rem minus doloremque modi qui, beatae dignissimos dolorem numquam suscipit. Nobis ducimus ullam est vero eligendi.</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quasi similique fugit deserunt eligendi rem minus doloremque modi qui, beatae dignissimos dolorem numquam suscipit. Nobis ducimus ullam est vero eligendi.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller Display</h4>
-    <p class="headline">This is Headline</p>
-    <p class="subheading font-weight-bold">This is Subheading</p>
-    <p class="caption">This is a caption</p>
+    <v-btn class="pink white--text ml-3">Click me</v-btn>
+    <v-btn depressed color="pink ml-3">Click me</v-btn>
+    <v-btn text color="pink ml-3">Click me</v-btn>  
+    <v-btn primary class="ml-3">Click me</v-btn>
+    
+    <br><br>
+
+    <v-btn large class="pink white--text ml-3">
+      <v-icon large left>mdi-email</v-icon>
+      <span>email</span>
+    </v-btn>
+    
+    <v-btn small class="pink white--text ml-3">
+      <v-icon small left>mdi-email</v-icon>
+      <span>email</span>
+    </v-btn>
+
+    <v-btn class="pink white--text mx-3">
+      <span>email</span>
+      <v-icon right>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
   </div>
 </template>
 
