f48dbb70269515dbb83de02bbedbd1c688cfbcbe
Mamun <mamun.cse63@gmail.com>
2020-11-07 23:58:37 +0600

    List

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav>
     <v-app-bar flat>
       <v-app-bar-nav-icon color="grey" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      
       <v-toolbar-title>
         <span class="font-weight-light">Todo</span>
         <span>Vuetify</span>
@@ -14,7 +15,16 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app temporary class="primary">
-      <p class="info">App</p>
+      <v-list>
+        <v-list-item v-for="link in links" :key="link" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -25,7 +35,12 @@ export default {
   name: 'Navbar',
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+        { icon: 'mdi-account', text: 'Team', route: '/team' },
+      ]
     }
   }
 }
