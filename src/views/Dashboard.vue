a9af48205fc50eddad1cea15e3e2708e24a54028
Mamun <mamun.cse63@gmail.com>
2020-11-08 14:59:04 +0600

    Grid(part 1)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,8 +3,30 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container fluid class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sint perferendis aut dolorem dolor id hic fuga. Praesentium, accusantium atque deleniti ratione labore totam modi illum quae! Esse, sapiente facere?</p>
-      <p>Fluid for taking full width/ viewport</p>
+      <v-row>
+        <v-col cols='12' md='6'>
+          <v-btn outlined block color="primary">1</v-btn>
+        </v-col>
+        <v-col cols='4' md='2'>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+        <v-col cols='4' md='2'>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+        <v-col cols='4' md='2'>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+      </v-row>
+
+      <!-- justify-center, justify-end, justify-space-around, justify-space-between -->
+      <v-row class="justify-space-around">
+        <v-col cols='4' md='3'>
+          <v-btn outlined block color="primary">1</v-btn>
+        </v-col>
+        <v-col cols='4' md='3'>
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
 
   </div>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,8 +3,11 @@
     <h1 class="subtitle-1 grey--text">Team</h1>
 
     <v-container fluid class="my-5">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sint perferendis aut dolorem dolor id hic fuga. Praesentium, accusantium atque deleniti ratione labore totam modi illum quae! Esse, sapiente facere?</p>
-      <p>Fluid for taking full width/ viewport</p>
+      <v-row>
+        <v-col>
+          <v-btn outlined block class="primary">1</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
 
   </div>
