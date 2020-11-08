49a8a30050568341b4195cff5a0dcb85de6cce6f
Mamun <mamun.cse63@gmail.com>
2020-11-08 15:40:20 +0600

    Grid(part 2)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,30 +3,26 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container fluid class="my-5">
-      <v-row>
-        <v-col cols='12' md='6'>
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols='4' md='2'>
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-        <v-col cols='4' md='2'>
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-        <v-col cols='4' md='2'>
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-      </v-row>
-
-      <!-- justify-center, justify-end, justify-space-around, justify-space-between -->
-      <v-row class="justify-space-around">
-        <v-col cols='4' md='3'>
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols='4' md='3'>
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-      </v-row>
+      <v-card class="pa-3">
+        <v-row>
+          <v-col cols='12' md='6'>
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
+          </v-col>
+          <v-col cols='6' sm='4' md='2'>
+            <div class="caption grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-col>
+          <v-col cols='6' sm='4' md='2'>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Nov 2020</div>
+          </v-col>
+          <v-col cols='2' sm='4' md='2'>
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
 
   </div>
