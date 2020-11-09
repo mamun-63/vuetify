c93c5129b969520537513c2b19c7c2bbff25b73a
Mamun <mamun.cse63@gmail.com>
2020-11-09 16:28:52 +0600

    Date pickers

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,42 +1,83 @@
 <template>
   <v-dialog max-width="600px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="success"
-          v-bind="attrs"
-          v-on="on"
-        >
-          Add new project
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <h2>Add a new project</h2>
-        </v-card-title>
-        <v-card-text>
-          <v-form class="px-3">
-            <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
-            <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil-outline"></v-textarea>
-            <v-btn text class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
-          </v-form>
-        </v-card-text>
-      </v-card>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="success" v-bind="attrs" v-on="on">
+        Add new project
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field
+            label="Title"
+            v-model="title"
+            prepend-icon="mdi-folder"
+          ></v-text-field>
+          <v-textarea
+            label="Information"
+            v-model="content"
+            prepend-icon="mdi-pencil-outline"
+          ></v-textarea>
+
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            max-width="290"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                :value="computedDateFormattedMomentjs"
+                prepend-icon="mdi-calendar"
+                clearable
+                label="Due date"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+                @click:clear="date = null"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              @change="menu = false"
+            ></v-date-picker>
+          </v-menu>
+
+          <v-spacer></v-spacer>
+          <v-btn text class="success mx-0 mt-3" @click="submit"
+            >Add Project</v-btn
+          >
+        </v-form>
+      </v-card-text>
+    </v-card>
   </v-dialog>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'Popup',
   data() {
     return {
       title: '',
       content: '',
-  }
-},
-methods: {
-  submit() {
-    console.log(this.title, this.content)
-  }
-}
+      date: null,
+      menu: false,
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content)
+    },
+  },
+
+  computed: {
+    computedDateFormattedMomentjs() {
+      return this.date ? moment(this.date).format('Do MMM YYYY') : ''
+    },
+  },
 }
 </script>
