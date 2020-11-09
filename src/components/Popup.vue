48b1ab0b6c7e6babeb2c71cdb90d20e24798fd7a
Mamun <mamun.cse63@gmail.com>
2020-11-09 13:23:02 +0600

    Form basics

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -13,13 +13,30 @@
         <v-card-title>
           <h2>Add a new project</h2>
         </v-card-title>
-        
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+            <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil-outline"></v-textarea>
+            <v-btn text class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
   </v-dialog>
 </template>
 
 <script>
 export default {
-  name: 'Popup'
+  name: 'Popup',
+  data() {
+    return {
+      title: '',
+      content: '',
+  }
+},
+methods: {
+  submit() {
+    console.log(this.title, this.content)
+  }
+}
 }
 </script>
